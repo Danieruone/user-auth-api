19f035d5f4dd3267a822ddf2ece13bb44bb53cc1
Daniel <danieruone@gmail.com>
2021-01-07 15:17:05 -0500

    jwt verification

--- server/config/config.js
+++ server/config/config.js
@@ -5,5 +5,5 @@ process.env.PORT = process.env.PORT || 3000;
 // database
 process.env.MONGO_URL = process.env.MONGO_URL || mongoURL;
 // token
-process.env.TOKEN_EXPIRATION = 60 * 60;
+process.env.TOKEN_EXPIRATION = 60 * 60 * 24 * 30;
 process.env.TOKEN_SEED = process.env.TOKEN_SEED || tokenSeed;
